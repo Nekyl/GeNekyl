b4dd8656029c136ed0a6032edf492d3da9340c1a
Nekyl <88288807+Nekyl@users.noreply.github.com>
2025-09-07 13:18:16 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -6,6 +6,7 @@ const STATIC_ASSETS = [
   './',
   './index.html',
   './manifest.json',
+  './pass.json',
   // CDN resources que serão cacheados quando acessados
   'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css',
   'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css',
